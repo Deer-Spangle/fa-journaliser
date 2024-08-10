2c52c73dcd0fbd733e75e8e983d0264b0a55a54c
Deer-Spangle <deer@spangle.org.uk>
2024-08-11 00:57:17 +0100

    Adding JournalInfo.check_errors() and implementing a bunch of error messages

--- journal_info.py
+++ journal_info.py
@@ -1,10 +1,31 @@
 import dataclasses
 import datetime
+import re
 from functools import cached_property
 from typing import Optional
 
 import dateutil.parser
 import bs4
+
+
+class JournalNotFound(Exception):
+    pass
+
+
+class FASystemError(Exception):
+    pass
+
+
+class RegisteredUsersOnly(Exception):
+    pass
+
+
+class AccountDisabled(Exception):
+    pass
+
+
+class PendingDeletion(Exception):
+    pass
 
 
 @dataclasses.dataclass
@@ -14,7 +35,6 @@ class JournalInfo:
 
     @classmethod
     def from_content(cls, journal_id: int, content: str) -> "JournalInfo":
-        print(f"Start of content: {content[:50]}")
         soup = bs4.BeautifulSoup(content, "html.parser")
         return JournalInfo(journal_id, soup)
 
@@ -22,23 +42,79 @@ class JournalInfo:
     def page_title(self) -> str:
         return self.soup.select_one("title").string
 
+    def check_errors(self) -> None:
+        if self.journal_deleted:
+            raise JournalNotFound()
+        if self.is_system_error:
+            raise FASystemError(f"System error: {self.error_message}")
+        if self.account_private:
+            raise RegisteredUsersOnly()
+        if self.account_disabled_username:
+            raise AccountDisabled(f"Account disabled: {self.account_disabled_username}")
+        if self.pending_deletion:
+            raise PendingDeletion()
+
     @cached_property
     def is_system_error(self) -> bool:
         return self.page_title == "System Error"
 
     @cached_property
-    def journal_exists(self) -> bool:
+    def journal_deleted(self) -> bool:
         if self.is_system_error:
-            return "you are trying to find is not in our database." in self.error_message
+            return "The journal you are trying to find is not in our database." in self.error_message
         return False
 
     @cached_property
-    def error_message(self) -> Optional[str]:
-        # TODO: this will be wrong
-        error_elem = self.soup.select_one("table.maintable td.alt1 font")
-        if error_elem:
-            return error_elem.string
+    def account_private(self) -> bool:
+        notice_message = self.site_content.select_one("section.notice-message")
+        if notice_message is None:
+            return False
+        redirect = notice_message.select_one(".redirect-message")
+        if redirect is None:
+            return False
+        return "The owner of this page has elected to make it available to registered users only." in redirect.strings
+
+    @cached_property
+    def account_disabled_username(self) -> Optional[str]:
+        """
+        If the page says the account is disabled, return the username of the disabled account. Otherwise return None.
+        """
+        notice_message = self.site_content.select_one("section.notice-message")
+        if notice_message is None:
+            return None
+        redirect = notice_message.select_one(".redirect-message")
+        if redirect is None:
+            return None
+        disabled_regex = re.compile(
+            r'User "([^"]+)" has voluntarily disabled access to their account and all of its contents.'
+        )
+        for redirect_string in redirect.stripped_strings:
+            match = disabled_regex.match(redirect_string)
+            if match is not None:
+                return match.group(1)
         return None
+
+    @cached_property
+    def pending_deletion(self) -> bool:
+        notice_message = self.site_content.select_one("section.notice-message")
+        if notice_message is None:
+            return False
+        redirect = notice_message.select_one("p.link-override")
+        if redirect is None:
+            return False
+        deletion_msg = "The page you are trying to reach is currently pending deletion by a request from its owner."
+        return deletion_msg in redirect.strings
+
+    @cached_property
+    def error_message(self) -> Optional[str]:
+        error_elem = self.soup.select_one(".section-body")
+        if error_elem:
+            return " ".join(error_elem.stripped_strings)
+        return None
+
+    @cached_property
+    def site_content(self) -> bs4.element.Tag:
+        return self.soup.select_one("#site-content")
 
     @cached_property
     def content(self) -> bs4.element.Tag:
