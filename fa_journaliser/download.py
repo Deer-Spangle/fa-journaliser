9d248d7b14d345e87d0cccf5ebf20e1430ee7911
Deer-Spangle <deer@spangle.org.uk>
2024-08-12 08:21:54 +0100

    Add a download method that falls back to cookies if it's registered users only

--- fa_journaliser/download.py
+++ fa_journaliser/download.py
@@ -1,27 +1,40 @@
 import asyncio
+import datetime
 import os
 import logging
+from typing import Optional
 
 import aiohttp
 
 from fa_journaliser.journal import Journal
+from fa_journaliser.journal_info import RegisteredUsersOnly, JournalInfo
 from fa_journaliser.utils import list_downloaded_journals
 
 logger = logging.getLogger(__name__)
 
 
-async def download_journal(journal_id: int) -> Journal:
+async def download_journal(journal_id: int, cookies: Optional[dict] = None) -> Journal:
     journal_url = f"https://www.furaffinity.net/journal/{journal_id}"
-    session = aiohttp.ClientSession()
-    journal = Journal(journal_id)
+    session = aiohttp.ClientSession(cookies=cookies)
+    journal = Journal(journal_id, datetime.datetime.now())
     async with session.get(journal_url) as resp:
         resp.raise_for_status()
         filename = journal.journal_html_filename
         os.makedirs(os.path.dirname(filename), exist_ok=True)
+        content = b""
         with open(filename, "wb") as f:
             async for chunk in resp.content.iter_chunked(8192):
+                content += chunk
                 f.write(chunk)
+        journal._info = JournalInfo.from_content_bytes(journal_id, content)
         return journal
+
+
+async def download_journal_with_backup_cookies(journal_id: int, cookies: dict) -> Journal:
+    try:
+        return await download_journal(journal_id)
+    except RegisteredUsersOnly:
+        return await download_journal(journal_id, cookies)
 
 
 async def work_forwards(start_journal: Journal) -> None:
