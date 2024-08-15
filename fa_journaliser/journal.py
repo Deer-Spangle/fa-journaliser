f73a20d6b2bc2ad877156eb562f4b7339cc69afa
Deer-Spangle <deer@spangle.org.uk>
2024-08-15 19:30:29 +0100

    Add a metric on which types of journal error page are saved

--- fa_journaliser/journal.py
+++ fa_journaliser/journal.py
@@ -3,15 +3,26 @@ import datetime
 import json
 import logging
 import pathlib
-from typing import Optional
+from typing import Optional, Type
 
 import aiofiles
 import aiofiles.os
+import prometheus_client
 
 from fa_journaliser.database import Database
 from fa_journaliser.journal_info import JournalInfo, JournalNotFound, AccountDisabled, PendingDeletion
 
 logger = logging.getLogger(__name__)
+
+
+total_error_counts = prometheus_client.Counter(
+    "fajournaliser_saved_journal_error_type_count_total",
+    "Total number of journal error pages seen when saving journals to database, by error type",
+    labelnames=["error_class"],
+)
+# Initialise for all error types that are saved
+for exc_class in [None, JournalNotFound, AccountDisabled, PendingDeletion]:
+    total_error_counts.labels(error_class=str(exc_class))
 
 
 @dataclasses.dataclass
@@ -69,25 +80,30 @@ class Journal:
         is_deleted = False
         archive_date = await self.archive_date()
         error = None
+        error_type: Optional[Type] = None
         login_used = info.login_user
         json_data = None
 
         logger.info("Saving journal link: %s", self.journal_link)
         try:
             info.check_errors()
-        except JournalNotFound:
+        except JournalNotFound as e:
             is_deleted = True
             error = "Journal not found"
+            error_type = type(e)
             logger.info("Journal not found")
         except AccountDisabled as e:
             is_deleted = True
             error = str(e)
+            error_type = type(e)
             logger.info(f"Account disabled: {e}")
         except PendingDeletion as e:
             is_deleted = True
             error = str(e)
+            error_type = type(e)
             logger.info(f"Account pending deletion: {e}")
         else:
             json_data = json.dumps(info.to_json())
             logger.info("Journal title: %s", info.title)
         await db.add_entry(journal_id, is_deleted, archive_date, error, login_used, json_data)
+        total_error_counts.labels(error_type=str(error_type)).inc()
