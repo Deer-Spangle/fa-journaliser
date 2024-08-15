584e293cd735dc2616205ee4c957eaf7ea8ddd21
Deer-Spangle <deer@spangle.org.uk>
2024-08-15 19:22:46 +0100

    Adding prometheus_client dependency and a tonne of metrics

--- fa_journaliser/database.py
+++ fa_journaliser/database.py
@@ -2,7 +2,14 @@ import datetime
 from typing import Optional
 
 import aiosqlite
+import prometheus_client
 from aiosqlite import Connection
+
+
+total_journal_db_entries = prometheus_client.Gauge(
+    "fajournaliser_database_journal_entries_total",
+    "Total number of journal entries in the database",
+)
 
 
 class Database:
@@ -22,10 +29,20 @@ class Database:
             PRIMARY KEY (`journal_id`)
         );""")
         await self.db.commit()
+        entry_count = await self.count_journals()
+        total_journal_db_entries.set(entry_count)
 
     async def stop(self) -> None:
         if self.db is not None:
             await self.db.close()
+
+    async def count_journals(self) -> int:
+        async with aiosqlite.connect(...) as db:
+            db.row_factory = aiosqlite.Row
+            async with db.execute("SELECT COUNT(*) AS count FROM journals") as cursor:
+                async for row in cursor:
+                    count = row['count']
+        return count
 
     async def add_entry(
             self,
@@ -47,3 +64,4 @@ class Database:
             )
         )
         await self.db.commit()
+        total_journal_db_entries.inc(1)
--- fa_journaliser/download.py
+++ fa_journaliser/download.py
@@ -8,6 +8,7 @@ from typing import Optional
 import aiofiles
 import aiofiles.os
 import aiohttp
+import prometheus_client
 
 from fa_journaliser.database import Database
 from fa_journaliser.journal import Journal
@@ -19,27 +20,118 @@ logger = logging.getLogger(__name__)
 BATCH_SIZE = 5
 
 
+total_web_requests = prometheus_client.Counter(
+    "fajournaliser_web_request_total",
+    "Total number of web requests which were made",
+    labelnames=["has_cookies"],
+)
+web_request_timing_histogram = prometheus_client.Histogram(
+    "fajournaliser_web_request_time_taken_seconds",
+    "Histogram of the time taken by each complete web request, from request to content complete. In seconds.",
+    buckets=[0.1, 0.25, 0.5, 0.75, 1, 1.5, 2, 5, 10],
+)
+total_downloaded_bytes = prometheus_client.Counter(
+    "fajournaliser_total_downloaded_bytes",
+    "Total amount of bytes downloaded from FA",
+    labelnames=["has_cookies"],
+)
+total_downloaded_pages = prometheus_client.Counter(
+    "fajournaliser_downloaded_pages_total",
+    "Total number of pages downloaded from FA",
+    labelnames=["has_cookies"],
+)
+download_attempts_needed = prometheus_client.Histogram(
+    "fajournaliser_download_attempts_needed_total",
+    "Number of web requests needed to successfully download a page",
+    labelnames=["has_cookies"],
+    buckets=[1, 2, 3, 4, 5],
+)
+total_downloaded_journals = prometheus_client.Counter(
+    "fajournaliser_downloaded_journals_total",
+    "Total number of journals which were archived",
+    labelnames=["needed_login"]
+)
+batch_download_timing_histogram = prometheus_client.Histogram(
+    "fajournaliser_batch_download_time_taken_seconds",
+    "Histogram of the time taken per batch of downloads, in seconds.",
+    labelnames=["batch_size"],
+    buckets=[0.1, 0.5, 1, 1.5, 2, 3, 4, 5, 10],
+)
+batch_save_timing_histogram = prometheus_client.Histogram(
+    "fa_journaliser_batch_save_time_taken_seconds",
+    "Histogram of the time taken to save a batch of journal entries, in seconds.",
+    labelnames=["batch_size"],
+    buckets=[0.1, 0.5, 1, 1.5, 2, 3, 4, 5, 10],
+)
+work_forwards_batch_size = prometheus_client.Gauge(
+    "fajournaliser_work_forwards_batch_size",
+    "Batch size being used for working forwards",
+)
+work_forwards_last_good_id = prometheus_client.Gauge(
+    "fajournaliser_work_forwards_last_good_journal_id",
+    "The newest journal ID that has been ingested while working forwards",
+)
+work_forwards_total_new_journals = prometheus_client.Counter(
+    "fajournaliser_work_forwards_total_new_journals",
+    "Count of how many new journal pages were archived while working forwards",
+)
+work_forwards_wasted_downloads = prometheus_client.Counter(
+    "fajournaliser_work_forwards_wasted_download_total",
+    "The total number of downloads done while working forwards which were then deleted (i.e. downloading journal "
+    "pages that don't yet exist)",
+)
+work_forwards_empty_batch_count = prometheus_client.Counter(
+    "fajournaliser_work_forwards_empty_batch_total",
+    "The total number of empty batches that were downloaded, where a whole batch of new journals do not exist yet",
+)
+work_backwards_batch_size = prometheus_client.Gauge(
+    "fajournaliser_work_backwards_batch_size",
+    "Batch size being used for working backwards",
+)
+work_backwards_oldest_id = prometheus_client.Gauge(
+    "fajournaliser_work_backwards_oldest_journal_id",
+    "The oldest journal ID that has been ingested while working backwards",
+)
+total_journal_files = prometheus_client.Gauge(
+    "fajournaliser_archived_journal_files_total",
+    "The total number of journal files archived",
+)
+
+
 async def download_journal(journal_id: int, cookies: Optional[dict] = None) -> Journal:
+    req_count = 0
+    cookie_label = str(cookies is not None)
     while True:
         session = aiohttp.ClientSession(cookies=cookies)
         journal = Journal(journal_id, datetime.datetime.now())
-        async with session.get(journal.journal_link) as resp:
-            try:
-                resp.raise_for_status()
-            except aiohttp.ClientError as e:
-                logger.warning("Web request failed for journal %s, retrying", journal.journal_link, exc_info=e)
-                await asyncio.sleep(5)
-                continue
-            filename = journal.journal_html_filename
-            dirname = os.path.dirname(filename)
-            await aiofiles.os.makedirs(dirname, exist_ok=True)
-            content = b""
-            async with aiofiles.open(filename, "wb") as f:
-                async for chunk in resp.content.iter_chunked(8192):
-                    content += chunk
-                    await f.write(chunk)
-            journal._info = JournalInfo.from_content_bytes(journal_id, content)
-            return journal
+        with web_request_timing_histogram.time():
+            async with session.get(journal.journal_link) as resp:
+                req_count += 1
+                total_web_requests.labels(has_cookies=cookie_label).inc()
+                # Check web request worked
+                try:
+                    resp.raise_for_status()
+                except aiohttp.ClientError as e:
+                    logger.warning("Web request failed for journal %s, retrying", journal.journal_link, exc_info=e)
+                    await asyncio.sleep(5)
+                    continue
+                # Prepare directory
+                filename = journal.journal_html_filename
+                dirname = os.path.dirname(filename)
+                await aiofiles.os.makedirs(dirname, exist_ok=True)
+                # Download content
+                content = b""
+                async with aiofiles.open(filename, "wb") as f:
+                    async for chunk in resp.content.iter_chunked(8192):
+                        content += chunk
+                        await f.write(chunk)
+                journal._info = JournalInfo.from_content_bytes(journal_id, content)
+                # Metrics
+                download_attempts_needed.labels(has_cookies=cookie_label).observe(req_count)
+                total_downloaded_pages.labels(has_cookies=cookie_label).inc()
+                total_downloaded_bytes.labels(has_cookies=cookie_label).inc(len(content))
+                # Return the journal
+                return journal
 
 
 async def download_journal_with_backup_cookies(journal_id: int, cookies: dict) -> Journal:
@@ -47,6 +139,8 @@ async def download_journal_with_backup_cookies(journal_id: int, cookies: dict) -
     info = await journal.info()
     if info.account_private:
         journal = await download_journal(journal_id, cookies)
+    total_downloaded_journals.labels(needed_login=str(info.account_private)).inc()
+    total_journal_files.inc()
     return journal
 
 
@@ -64,17 +158,20 @@ async def download_if_not_exists(db: Database, journal_id: int, cookies: dict) -
 
 
 async def download_many(journal_ids: list[int], cookies: dict) -> list[Journal]:
-    return list(await asyncio.gather(*[
-        download_journal_with_backup_cookies(journal_id, cookies) for journal_id in journal_ids
-    ]))
+    with batch_download_timing_histogram.labels(batch_size=str(len(journal_ids))).time():
+        return list(await asyncio.gather(*[
+            download_journal_with_backup_cookies(journal_id, cookies) for journal_id in journal_ids
+        ]))
 
 
 async def save_many(journals: list[Journal], db: Database) -> None:
-    await asyncio.gather(*[journal.save(db) for journal in journals])
+    with batch_save_timing_histogram.labels(batch_size=str(len(journals))).time():
+        await asyncio.gather(*[journal.save(db) for journal in journals])
 
 
 async def delete_many(journals: list[Journal]) -> None:
     await asyncio.gather(*[aiofiles.os.remove(j.journal_html_filename) for j in journals])
+    total_journal_files.inc(-len(journals))
 
 
 async def work_forwards(
@@ -84,6 +181,7 @@ async def work_forwards(
         max_id: Optional[int] = None,
         batch_size: int = BATCH_SIZE,
 ) -> None:
+    work_forwards_batch_size.set(batch_size)
     logger.info("Working forwards from %s, this is tricky.", start_journal)
     last_good_id = start_journal.journal_id
     while True:
@@ -106,18 +204,23 @@ async def work_forwards(
         good_journals = [next_journals[i] for i, info in enumerate(next_infos) if not info.journal_deleted]
         # If none of these journals exist, then wait and try again
         if len(good_journals) == 0:
+            work_forwards_empty_batch_count.inc()
             logger.warning("Didn't get any good new journals in that batch! Gonna wait and retry")
             await asyncio.sleep(30)
             continue
         # Convert to list of IDs and figure which is the bleeding edge newest journal
         good_ids = [j.journal_id for j in good_journals]
         last_good_id = max(good_ids)
+        work_forwards_last_good_id.set(last_good_id)
         # Figure out which were before the bleeding edge and which are after
         split_on_last_good = split_list(next_journals, lambda j: j.journal_id <= last_good_id)
         await asyncio.gather(
             save_many(split_on_last_good[True], db),
             delete_many(split_on_last_good[False])
         )
+        # Metrics and logging
+        work_forwards_total_new_journals.inc(len(split_on_last_good[True]))
+        work_forwards_wasted_downloads.inc(len(split_on_last_good[False]))
         saved_ids = [j.journal_id for j in split_on_last_good[True]]
         logger.info("Downloaded new journals: (%s) %s", len(saved_ids), saved_ids)
 
@@ -129,6 +232,7 @@ async def work_backwards(
         min_id: int = 0,
         batch_size: int = BATCH_SIZE,
 ) -> None:
+    work_backwards_batch_size.set(batch_size)
     logger.info("Working backwards from %s. I have the easy job", start_journal)
     current_journal = start_journal
     while True:
@@ -147,6 +251,7 @@ async def work_backwards(
         logger.info("Downloaded old journals %s", next_batch)
         # Figure out next ID to start from
         current_journal = min(next_journals, key=lambda x: x.journal_id)
+        work_backwards_oldest_id.set(current_journal.journal_id)
 
 
 async def run_download(
@@ -160,6 +265,7 @@ async def run_download(
 ) -> None:
     # List all current journals
     all_journals = list_downloaded_journals()
+    total_journal_files.set(len(all_journals))
     # Truncate the set of journals to min and max
     all_journals = [
         j for j in all_journals
